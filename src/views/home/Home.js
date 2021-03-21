0bda91e4f9a39de658d2450a0ad3cacd5a1ff16c
sushmit_windows <sushmitsarmah@gmail.com>
2021-03-21 10:22:26 +0530

    added styles

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 import logo from '../../assets/logo.svg';
+// import clam from '../../assets/img/clamjam/clam_bigmouth_pink_nopearl.png';
 import './Home.css';
 
 const Home = () => {
   return  <div className="App">
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
+      {/* <img src={clam} className="clam" alt="logo" /> */}
+      <img src={require('../../assets/img/clamjam/clam_bigmouth_pink_nopearl.png').default} className="clam" alt="logo" />
       <p>
         Edit <code>src/App.js</code> and save to reload.
       </p>
