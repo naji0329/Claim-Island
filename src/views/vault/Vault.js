2cacf1a81b628c8e3ac56d3887646c7a25f46ec3
Roman Semin <roma.semin@gmail.com>
2021-03-21 09:27:34 -0400

    vault items and nav bar styling

--- src/views/vault/Vault.js
+++ src/views/vault/Vault.js
@@ -1,60 +1,103 @@
 import React, { useState } from 'react';
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  NavLink as RRNavLink
-} from "react-router-dom";
+import AddClam from '../../assets/img/add_clam.png';
+
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import { Popover, PopoverHeader, PopoverBody } from 'reactstrap';
 
 import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
+  Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button,
   Container, Row, Col,
-  Button,
-  Card, CardImg, CardBody, CardTitle, CardSubtitle, CardText, CardColumns
+  CardColumns
 } from 'reactstrap';
 
 import logo from '../../assets/logo.svg';
 import './Vault.css';
 
+import { PEARLS, CLAMS } from '../../constants';
+
 const Vault = () => {
 
+  const [clams, setClams] = useState([]);
+
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const toggle = () => setDropdownOpen(prevState => !prevState);
+
+
+  const addClamToFarm = (clam) => {
+      // clams.push(clam);
+      setClams(k => [...k, clam]);
+      
+      // const [popoverOpen, setPopoverOpen] = useState(false);
+      // const toggle = () => setPopoverOpen(!popoverOpen);
+
+  };
+
+  const renderClamCard  = (clam) => {
+    return(
+      <Card>
+        <CardImg top style={{width: '200px', height: '200px'}} src={AddClam} alt="Card image cap" />
+        <CardBody style={{height: '200px'}}>
+            <CardTitle tag="h5">Deposit Clams</CardTitle>
+            <CardSubtitle tag="h6" className="mb-2 text-muted">A place to select and deposit Clams</CardSubtitle>
+            <CardText>Choose Clams from your Vault to deposit into the Farm</CardText>
+
+            <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+                <DropdownToggle caret  style={{backgroundColor: '#0072E3'}}>
+                    Select Clams
+                </DropdownToggle>
+                <DropdownMenu>
+                    {CLAMS.map((k, i) => {
+                        return <DropdownItem key={i} onClick={() => addClamToFarm(k)}>
+                            <img style={{width: "50px", height: "50px"}} src={require(`../../assets/img/clamjam/${k.src}`).default}/>
+                            <span style={{fontFamily: "AristotelicaBold", fontSize: "16px"}}>{k.title}</span>
+                        </DropdownItem>;
+                    })}
+                    
+                  </DropdownMenu>
+              </Dropdown>
+          </CardBody>
+      </Card>
+    );
+  }
+
   return(
-    <Router>
       <div className="App">
         <h2 className='header'>Your Vault</h2>
-        <CardColumns>
-            <Card className='Card'>
-                <CardImg top width="100%" src="src\assets\clam-pic.jpg" alt="Your clam or pearl" />
-                <CardBody>
-                    <CardTitle tag="h5">Name of the NFT</CardTitle>
-                    {/* <CardSubtitle tag="h6" className="mb-2 text-muted">Balance:</CardSubtitle> */}
-                </CardBody>
-            </Card>
-            <Card className='Card'>
-                <CardImg top width="100%" src="src\assets\clams.jpg" alt="Your clam or pearl" />
-                <CardBody>
-                    <CardTitle tag="h5">Name of the NFT</CardTitle>
-                    {/* <CardSubtitle tag="h6" className="mb-2 text-muted">Balance:</CardSubtitle> */}
-                </CardBody>
-            </Card>
-            <Card className='Card'>
-                <CardImg top width="100%" src="src\assets\clams.jpg" alt="Your clam or pearl" />
-                <CardBody>
-                    <CardTitle tag="h5">Name of the NFT</CardTitle>
-                    {/* <CardSubtitle tag="h6" className="mb-2 text-muted">Balance:</CardSubtitle> */}
-                </CardBody>
-            </Card>
-        </CardColumns>
+        <div style={{textAlign: "#left"}}>
+          <h3>Clams</h3>
+          <CardColumns>
+              {CLAMS.map((k, i) => {
+                  const src = require(`../../assets/img/clamjam/${k.src}`).default;
+                  return <Card key={i}>
+                      <CardImg top style={{width: '200px', height: '200px'}} src={src} alt="Card image cap" />
+                      <CardBody style={{height: '200px'}}>
+                          <CardTitle tag="h5">{k.title}</CardTitle>
+                          <CardSubtitle tag="h6" className="mb-2 text-muted">This clam is in the farm</CardSubtitle>
+                          <CardText>This Clam has been selected for farming.</CardText>
+                      </CardBody>
+                  </Card>
+              })}
+            </CardColumns>
+          </div>
+          <div style={{textAlign: "#left"}}>
+            <h3>Pearls</h3>
+            <CardColumns>
+              {PEARLS.map((p, i) => {
+                  const src = require(`../../assets/img/clamjam/${p.src}`).default;
+                  return <Card key={i}>
+                      <CardImg top style={{width: '200px', height: '200px'}} src={src} alt="Card image cap" />
+                      <CardBody style={{height: '200px'}}>
+                          <CardTitle tag="h5">{p.title}</CardTitle>
+                          <CardSubtitle tag="h6" className="mb-2 text-muted">This clam is in the farm</CardSubtitle>
+                          <CardText>This Clam has been selected for farming.</CardText>
+                      </CardBody>
+                  </Card>
+              })}
+            </CardColumns>
+          </div>
       </div>
-    </Router>
   );
 };
 
