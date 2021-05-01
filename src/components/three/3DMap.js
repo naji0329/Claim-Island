b9d67770eb026b8b441a0893ed67730d45e9e91a
sushmit_windows <sushmitsarmah@gmail.com>
2021-05-01 16:34:44 +0530

    updated models and added shadow maps

--- src/components/three/3DMap.js
+++ src/components/three/3DMap.js
@@ -33,6 +33,10 @@ const Map3D = (props) => {
 const create3DScene = (element) => {
     // create a 3d renderer
     const renderer = new THREE.WebGLRenderer();
+    renderer.shadowMap.enabled = true;
+    // renderer.gammaOutput = true;
+    // renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
+    // renderer.gammaFactor = 2.2;
     renderer.setSize( window.innerWidth, window.innerHeight );
     
 
@@ -45,11 +49,11 @@ const create3DScene = (element) => {
     // create a camera
     // const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
     // camera.position.set( 0, 2, 5 );
-    // camera.lookAt( 0, 0, 0 );
     const camera = new THREE.PerspectiveCamera( 55, window.innerWidth / window.innerHeight, 1, 20000 );
-	camera.position.set( 30, 30, 100 );
+	camera.position.set( 730, 380, 700 );
     // camera.position.set( 1, 1, 20 );
     // camera.position.set( 0, 2, 5 );
+    camera.lookAt( 0, 0, 0 );
 
     // orbit controls to pan and zoom
     const controls = new OrbitControls( camera, renderer.domElement );
@@ -70,45 +74,132 @@ const create3DScene = (element) => {
     // ISLAND_OBJECTS.forEach((obj) => {
     //     loadTexture({ ...obj, scene });
     // });
+    // loadGLTF('glb_files/Island_Empty.glb', scene);
     loadGLTF('glb_files/Bank_Island.glb', scene);
-    loadGLTF('glb_files/Farm_Island.glb', scene);
+    loadGLTF('glb_files/farm_island.glb', scene);
     loadGLTF('glb_files/Market_Island.glb', scene);
     loadGLTF('glb_files/Vault_Island.glb', scene);
     loadGLTF('glb_files/Bridges.glb', scene);
     loadGLTF('glb_files/Rocks.glb', scene);
     loadGLTF('glb_files/LillyPads.glb', scene);
+    loadGLTF('glb_files/Boats.glb', scene);
+    loadGLTF('glb_files/ship.glb', scene, 'ship');
+    loadGLTF('glb_files/sailboat.glb', scene, 'sailboat');
+    loadGLTF('glb_files/seagull.glb', scene, 'seagull');
+    loadGLTF('glb_files/dolphin.glb', scene, 'dolphin');
 
     animate({
         scene,
         water,
         camera,
         controls,
-        renderer
+        renderer,
     });
 };
 
 // ADD LIGHTS
 const addLights = (scene) => {
-    const ambient = new THREE.AmbientLight( 0xffffff );
-    scene.add( ambient );
+    // const ambient = new THREE.AmbientLight( 0xffffff );
+    // scene.add( ambient );
 
-    const directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
-    directionalLight.position.set( 0, 10, 0 ).normalize();
+    const directionalLight = new THREE.DirectionalLight( 0xffffff, 2 );
+    directionalLight.position.set( 140, 10, 0 ).normalize();
+    directionalLight.castShadow = true;
     scene.add( directionalLight );
+
+    const directionalLight2 = new THREE.DirectionalLight( 0xffffff, 2 );
+    directionalLight2.position.set( -190, 10, 0 ).normalize();
+    directionalLight2.castShadow = true;
+    scene.add( directionalLight2 );
 };
 
-const loadGLTF = (url, scene) => {
+const loadGLTF = (url, scene, type='island') => {
     const loader = new GLTFLoader();
     loader.load(url, (gltf) => {
-        console.log(gltf)
         // scene.add(gltf);
+        const children = gltf.scene.children.map(obj => {
+            obj.traverse( function ( child ) {
+                if ( child instanceof THREE.Mesh ) {
+                    // console.log(child);
+                    child.castShadow = true;
+                    child.receiveShadow = true;
+                }
+            });
+            return obj;
+        });
+        gltf.scene.children = children;
 
-        gltf.scene.scale.set( 500, 500, 500 );			   
-        gltf.scene.position.x = 0;				    //Position (x = right+ left-) 
-        gltf.scene.position.y = 10;				    //Position (y = up+, down-)
-        gltf.scene.position.z = 0;	
+        if(type === 'island') {
+            gltf.scene.scale.set( 1000, 1000, 1000 );			   
+            gltf.scene.position.x = 0;				    //Position (x = right+ left-) 
+            gltf.scene.position.y = 2;				    //Position (y = up+, down-)
+            gltf.scene.position.z = 0;	
+        } else if(type === 'sailboat') {
+            gltf.scene.scale.set( 30, 30, 30 );			   
+            gltf.scene.position.x = 0;				    //Position (x = right+ left-) 
+            gltf.scene.position.y = 60;				    //Position (y = up+, down-)
+            gltf.scene.position.z = 600;	
+            gltf.scene.rotation.y = 5;	
+        } else if(type === 'seagull') {
+            gltf.scene.scale.set( 5, 5, 5 );			   
+            gltf.scene.position.x = 0;				    //Position (x = right+ left-) 
+            gltf.scene.position.y = 500;				    //Position (y = up+, down-)
+            gltf.scene.position.z = 0;	
 
-        scene.add( gltf.scene );
+            for(let i = 0; i < 10; i++) {
+                const clone = gltf.scene.clone();
+                // console.log(clone)
+                if(clone) {
+                    clone.scale.set( 10, 10, 10 );			   
+                    clone.position.z = i*250*Math.random();
+                    clone.position.x = i*250*Math.random();
+                    clone.position.y = 100 + i*250*Math.random();				    //Position (y = up+, down-)
+                    scene.add(clone);
+                }
+            }
+
+        } else if(type === 'dolphin') {
+            gltf.scene.scale.set( 10, 10, 10 );			   
+            gltf.scene.position.x = 0;				    //Position (x = right+ left-) 
+            gltf.scene.position.y = 10;				    //Position (y = up+, down-)
+            gltf.scene.position.z = -500;
+            // scene.add(gltf.scene);
+            // const obj = gltf.scene.children[0];
+            for(let i = 0; i < 10; i++) {
+                const clone = gltf.scene.clone();
+                // console.log(clone)
+                if(clone) {
+                    clone.scale.set( 10, 10, 10 );			   
+                    clone.position.z = -600 + i*50*Math.random();
+                    clone.position.x = i*-20;
+                    scene.add(clone);
+                }
+            }
+        } else {
+            gltf.scene.scale.set( 500, 500, 500 );			   
+            gltf.scene.position.x = 0;				    //Position (x = right+ left-) 
+            gltf.scene.position.y = 15;				    //Position (y = up+, down-)
+            gltf.scene.position.z = 20;	
+        }
+
+        // console.log(gltf.scene)
+
+        // let mixer;
+
+        // if(gltf.animations && gltf.animations.length > 0) {
+        //     mixer = new THREE.AnimationMixer( gltf.scene );
+        //     var action = mixer.clipAction( gltf.animations[ 0 ] );
+        //     action.play();
+        //     scene.add( gltf.scene );
+        // } else {
+        if(['dolphin', 'seagull'].indexOf(type) === -1) {
+            scene.add( gltf.scene );
+        }
+        // }
+
+        // return mixer;
+
+        // scene.add( gltf.scene );
 
 		// gltf.animations; // Array<THREE.AnimationClip>
 		// gltf.scene; // THREE.Group
@@ -241,7 +332,7 @@ const createWater = ({ scene }) => {
             } ),
             sunDirection: new THREE.Vector3(),
             sunColor: 0xffffff,
-            waterColor: 0x001e0f,
+            waterColor: 0x00FFFF,
             distortionScale: 3.7,
             fog: scene.fog !== undefined
         }
@@ -262,12 +353,12 @@ const createSky = ({ scene, water, sun, renderer }) => {
 
     skyUniforms[ 'turbidity' ].value = 10;
     skyUniforms[ 'rayleigh' ].value = 2;
-    skyUniforms[ 'mieCoefficient' ].value = 0.005;
-    skyUniforms[ 'mieDirectionalG' ].value = 0.8;
+    skyUniforms[ 'mieCoefficient' ].value = 0.003;
+    skyUniforms[ 'mieDirectionalG' ].value = 1;
 
     const parameters = {
-        inclination: 0.47,
-        azimuth: 0.205
+        inclination: 0.37,
+        azimuth: 0.445
     };
 
     const pmremGenerator = new THREE.PMREMGenerator( renderer );
@@ -280,6 +371,7 @@ const createSky = ({ scene, water, sun, renderer }) => {
         sun.x = Math.cos( phi );
         sun.y = Math.sin( phi ) * Math.sin( theta );
         sun.z = Math.sin( phi ) * Math.cos( theta );
+        sun.castShadow = true;
 
         sky.material.uniforms[ 'sunPosition' ].value.copy( sun );
         water.material.uniforms[ 'sunDirection' ].value.copy( sun ).normalize();
--- src/services/web3.js
+++ src/services/web3.js
@@ -1,9 +1,9 @@
 import Web3 from 'web3';
 
-import * as ClamToken from '../contracts/Clam.json';
-import * as PearlToken from '../contracts/PearlCore.json';
-import * as ClamCore from '../contracts/ClamNftToken.json';
-import * as PearlCore from '../contracts/PearlNftToken.json';
+// import * as ClamToken from '../contracts/Clam.json';
+// import * as PearlToken from '../contracts/PearlCore.json';
+// import * as ClamCore from '../contracts/ClamNftToken.json';
+// import * as PearlCore from '../contracts/PearlNftToken.json';
 
 const CLAM_TOKEN = process.env.REACT_APP_CLAM_TOKEN;
 const PEARL_TOKEN = process.env.REACT_APP_PEARL_TOKEN;
@@ -23,32 +23,32 @@ const getContract = (abi, address, contractOptions) => {
   return new web3.eth.Contract(abi, address, contractOptions);
 };
 
-const getClamTokenContract = () => {
-  const contract = getContract(ClamToken.abi, CLAM_TOKEN, {});
-  return contract;
-};
+// const getClamTokenContract = () => {
+//   const contract = getContract(ClamToken.abi, CLAM_TOKEN, {});
+//   return contract;
+// };
 
-const getPearlTokenContract = () => {
-  const contract = getContract(PearlToken.abi, PEARL_TOKEN, {});
-  return contract;
-};
+// const getPearlTokenContract = () => {
+//   const contract = getContract(PearlToken.abi, PEARL_TOKEN, {});
+//   return contract;
+// };
 
-const getClamNFTContract = () => {
-  const contract = getContract(ClamCore.abi, CLAM_CORE, {});
-  return contract;
-};
+// const getClamNFTContract = () => {
+//   const contract = getContract(ClamCore.abi, CLAM_CORE, {});
+//   return contract;
+// };
 
-const getPearlNFTContract = () => {
-  const contract = getContract(PearlCore.abi, PEARL_CORE, {});
-  return contract;
-};
+// const getPearlNFTContract = () => {
+//   const contract = getContract(PearlCore.abi, PEARL_CORE, {});
+//   return contract;
+// };
 
 export {
   getWeb3,
   getContract,
-  getClamTokenContract,
-  getPearlTokenContract,
-  getClamNFTContract,
-  getPearlNFTContract,
+  // getClamTokenContract,
+  // getPearlTokenContract,
+  // getClamNFTContract,
+  // getPearlNFTContract,
   httpProvider
 };
