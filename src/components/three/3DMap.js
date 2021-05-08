3a961296cceec975f336e8f55e807b901a8d0415
sushmit_windows <sushmitsarmah@gmail.com>
2021-05-08 12:33:09 +0530

    clean up code

--- src/components/three/3DMap.js
+++ src/components/three/3DMap.js
@@ -120,32 +120,21 @@ const addLights = (scene) => {
     // scene.add( ambient );
 
     const directionalLight = new THREE.DirectionalLight( 0xffffff, 4 );
-    //directionalLight.position.set( 140, 10, 0 ).normalize();
     directionalLight.position.set( 2700, 2000, -900 );
     directionalLight.rotation.set(0, 0.3, -0.55);
     directionalLight.castShadow = true;
     directionalLight.shadow.camera.scale.set(500, 500, 500);
     directionalLight.shadow.mapSize.set(2048, 2048);
     scene.add( directionalLight );
-    //console.log(directionalLight);
 
     const directionalLight2 = new THREE.DirectionalLight( 0xffffff, 0.7 );
-    //directionalLight2.position.set( -190, 10, 0 ).normalize();
     directionalLight2.position.set( -2700, 2000, 900 );
     directionalLight2.rotation.set(0, 0.3, 0.6);
 
-    //directionalLight2.castShadow = true;
     scene.add( directionalLight2 );
-    //console.log(directionalLight2);
 
     const hemiLight = new THREE.HemisphereLight(0xffffff, 0xe0fffc, 0.25);
     scene.add(hemiLight);
-
-    //console.log(hemiLight);
- 
-    //scene.add( new THREE.DirectionalLightHelper( directionalLight, 5, 0xfc0317 ) );
-    //scene.add( new THREE.CameraHelper( directionalLight.shadow.camera, 5, 0x0022ff ) );
-    //scene.add( new THREE.DirectionalLightHelper( directionalLight2, 5, 0xfc0317 ) );
 };
 
 // CALL ANIMATE EVERY SECOND TO DISPLAY
@@ -202,10 +191,7 @@ const giveBuoyancy = (obj, t, factor, init) => {
 
 const flyingSeagulls = (seagulls, t) => {
     if(seagulls) {
-        //const axis = new THREE.Vector3(0, 1, 0);
         seagulls.forEach((seagull, i) => {
-                //const theta = THREE.Math.degToRad(15 * (i+1)) * 0.015;
-                //rotateAboutPoint(seagull.obj, seagull.pivot, axis, theta);
                 seagull.pivot.rotation.y += seagull.pivot.userData.speed + 0.01;
         });
     }
@@ -234,28 +220,5 @@ const swimmingDolphins = (dolphins, t) => {
         });
     }
 };
-
-// obj - your object (THREE.Object3D or derived)
-// point - the point of rotation (THREE.Vector3)
-// axis - the axis of rotation (normalized THREE.Vector3)
-// theta - radian value of rotation
-// pointIsWorld - boolean indicating the point is in world coordinates (default = false)
-function rotateAboutPoint(obj, point, axis, theta, pointIsWorld){
-    pointIsWorld = (pointIsWorld === undefined)? false : pointIsWorld;
-
-    if(pointIsWorld){
-        obj.parent.localToWorld(obj.position); // compensate for world coordinate
-    }
-
-    obj.position.sub(point); // remove the offset
-    obj.position.applyAxisAngle(axis, theta); // rotate the POSITION
-    obj.position.add(point); // re-add the offset
-
-    if(pointIsWorld){
-        obj.parent.worldToLocal(obj.position); // undo world coordinates compensation
-    }
-
-    obj.rotateOnAxis(axis, theta); // rotate the OBJECT
-}
 
 export default Map3D;
