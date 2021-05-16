379aec856e62690404629e0d6da46d8283eb1727
sushmit_windows <sushmitsarmah@gmail.com>
2021-05-16 11:16:00 +0530

    edited character button

--- src/components/characters/index.js
+++ src/components/characters/index.js
@@ -11,19 +11,28 @@ import Diego from '../../assets/characters/diego.png';
 
 import './index.scss';
 
+
+const SPEECHES = [
+    `G'day, welcome to Clam Island!
+    The island isn't open yet so you can't go into any of the buildings,
+    but feel free to have a look around while we're here.`,
+    `Nice place, isn't it? Not long before it opens now. I'll be sure to give you the inside scoop if I hear something.`
+    
+];
+
 const CharacterSpeak = (props) => {
     const [charImg, setCharImg] = useState(Nacre);
     const [charName, setCharName] = useState('Captain Nacre');
     const [showBubble, setShowBubble] = useState(true);
+    const [speech, setSpeech] = useState(SPEECHES[0]);
 
     const onClick = (e) => {
         setShowBubble(false);
     };
 
     const onClickBubble = (e) => {
-        console.log('#########');
-        // setShowBubble(true);
-        console.log(showBubble)
+        setShowBubble(true);
+        setSpeech(SPEECHES[1]);
     };
 
     return (
@@ -37,11 +46,7 @@ const CharacterSpeak = (props) => {
             <div className="text-bubble">
                 <div className="name">{charName}</div>
                 <div className="speech">
-                    <p className="speech-text">
-                        G'day, welcome to Clam Island!
-                        The island isn't open yet so you can't go into any of the buildings,
-                        but feel free to have a look around while we're here.
-                    </p>
+                    <p className="speech-text">{speech}</p>
                 </div>
                 <Button className="btn" onClick={onClick}>OK</Button>
             </div>
