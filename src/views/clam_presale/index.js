26bd3d6edb4221230fd972c9dd6ead352969f72a
Luis <hi@ernane.me>
2021-06-07 19:35:45 -0300

    🐛(FEB1) fix crash when metamask is not installed

--- src/views/clam_presale/index.js
+++ src/views/clam_presale/index.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import { useAsync } from "react-use";
 import "./index.scss";
 import CharacterSpeak from "../../components/characters";
 
@@ -21,25 +22,35 @@ const ClamPresale = () => {
 
   const web3 = getWeb3();
 
-  if (web3) {
-    setInterval(async () => {
-      const cap = await presaleCap();
-      const totalSupply = await totalClamSupply();
-      const prog = (Number(totalSupply) / cap) * 100;
-      setProgress(prog);
-    }, 3000);
-  }
+  useAsync(async () => {
 
-  useEffect(() => {
-    const hasSaleBegun = async function () {
+    if (web3) {
+      setInterval(async () => {
+        const cap = await presaleCap();
+        const totalSupply = await totalClamSupply();
+        const prog = (Number(totalSupply) / cap) * 100;
+        setProgress(prog);
+      }, 3000);
+
       const hasIt = await getHasSaleStarted();
       setHasSaleStarted(hasIt);
-    };
-    hasSaleBegun();
-  }, []);
+    }
+  })
 
   return (
     <>
+     {!web3 && (
+        <div class="bg-red-200 border-t-4 border-red-600 rounded-md text-red-800 px-4 py-3 shadow-xl m-2 absolute w-1/2 z-50" role="alert">
+        <div class="flex">
+          <svg class="h-6 w-6 fill-current text-red-500 mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg>
+          <div>
+            <p class="font-bold">You do not have installed any web3 wallet.</p>
+            <p class="text-sm ">Make sure you have one. We recommend <a target="_blank" rel="noreferrer" href="https://metamask.io/" class="underline" >metamask</a>.</p>
+          </div>
+        </div>
+      </div>
+     )}
+
       <Progress striped color="danger" value={progress}>
         {progress}% of Clams Purchased
       </Progress>
@@ -58,7 +69,8 @@ const ClamPresale = () => {
             progress={progress}
           />
         ) : (
-          ""
+          <>
+          </>
         )}
 
         {!hasSaleStarted && (
