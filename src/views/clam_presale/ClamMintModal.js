333f6c2dc735795a57c458266f50bf388c4ca7e7
Luis <hi@ernane.me>
2021-06-15 18:43:09 -0300

    fix design to look more similar to mockups

--- src/views/clam_presale/ClamMintModal.js
+++ src/views/clam_presale/ClamMintModal.js
@@ -5,6 +5,9 @@ import { connect } from "redux-zero/react";
 import "./index.scss";
 
 import Card from "../../components/Card";
+import ClamUnknown from "../../assets/img/clam_unknown.png";
+import BNBLogo from "../../assets/img/binance-coin-bnb-logo.png";
+import ArrowDown from "../../assets/img/arrow-down.svg";
 
 import { buyClamPresale } from "../../web3/buyClamPresale";
 import { clamPresaleAddress } from "../../web3/constants";
@@ -13,13 +16,7 @@ import { actions } from "../../store/redux";
 const Divider = () => (
   <div className="w-full flex flex-col justify-center items-center my-2">
     <div className="bg-grey-light hover:bg-grey text-grey-darkest font-bold py-2 px-4 rounded inline-flex items-center">
-      <svg
-        className="w-4 h-4 mr-2"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-      >
-        <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
-      </svg>
+      <img className="h-8 mr-2" src={ArrowDown} />
     </div>
   </div>
 );
@@ -65,57 +62,62 @@ const ClamMintModal = ({
       <form onSubmit={handleSubmit(onSubmit)}>
         <Card>
           <div className="flex flex-col mb-4">
-            <h2 className="text-blue-700 font-semibold text-2xl tracking-wide mb-2">
+            <h2 className="text-blue-700 text-center font-semibold text-3xl tracking-wide mb-2">
               Get Clams on BSC
             </h2>
-            <a
-              className="text-gray-500 text-base underline"
-              href={getExplorerAddressLink(clamPresaleAddress, ChainId.BSC)}
-              target="_blank"
-              rel="noreferrer"
-            >
-              {clamPresaleAddress}
-            </a>
+            {address ? (
+              <a
+                className="text-gray-500 text-base underline"
+                href={getExplorerAddressLink(clamPresaleAddress, ChainId.BSC)}
+                target="_blank"
+                rel="noreferrer"
+              >
+                {clamPresaleAddress}
+              </a>
+            ) : (
+              <span className="text-yellow-400 text-center">
+                Wallet not connected
+              </span>
+            )}
           </div>
 
           {/* input */}
-          <div className="bg-white border-2 shadow-xl rounded-lg">
-            <div className="px-2 py-1">
+          <div className="bg-white border-2 shadow rounded-xl">
+            <div className="px-2 py-2">
               <div className="flex flex-col">
-                <h4 className="text-lg font-semibold">Price of Clam</h4>
-                <div className="flex flex-col text-sm text-gray-500">
+                <h4 className="text-lg font-semibold my-2">Price of Clam</h4>
+                <div className="flex flex-col text-sm text-gray-600">
                   <div className="flex flex-col">
-                    <label
-                      htmlFor="price"
-                      className="mb-1 uppercase text-grey-darker text-xs font-bold"
-                    >
-                      amount
-                    </label>
-                    <div className="flex flex-row">
-                      <input
-                        disabled
-                        value={salePrice}
-                        className="bg-grey-lighter text-grey-darker p-2 font-normal rounded text-grey-darkest border border-grey-lighter rounded-l-none font-bold"
-                        {...register("input", { required: true })}
-                        // onChange={(v) => {
-                        //   const input = parseUnits(
-                        //     v.currentTarget.value,
-                        //     "wei"
-                        //   ); // input in wei
-                        //   const price = parseUnits(
-                        //     presaleState.salePrice,
-                        //     "ether"
-                        //   ); // input in ether
-                        //   const out = formatUnits(input.mul(price), 18);
-                        //   setValue("output", out);
-                        // }}
-                      />
-                      <span className="flex items-center bg-gray-100 rounded rounded-r-none px-3 text-lg font-bold font-sans mx-1">
-                        BNB
+                    <div className="flex flex-row items-center justify-between">
+                      <div className="flex">
+                        <img className="h-12 mr-2" src={BNBLogo} />
+                        <input
+                          disabled
+                          value={salePrice}
+                          className="bg-gray-100 text-center text-xl w-20  text-black p-2 font-normal rounded  border-none rounded-l-none font-extrabold"
+                          {...register("input", { required: true })}
+                          // onChange={(v) => {
+                          //   const input = parseUnits(
+                          //     v.currentTarget.value,
+                          //     "wei"
+                          //   ); // input in wei
+                          //   const price = parseUnits(
+                          //     presaleState.salePrice,
+                          //     "ether"
+                          //   ); // input in ether
+                          //   const out = formatUnits(input.mul(price), 18);
+                          //   setValue("output", out);
+                          // }}
+                        />
+                        <span className="flex items-center  px-3 text-lg font-extrabold font-sans mx-1">
+                          BNB
+                        </span>
+                      </div>
+                      <span className="my-2">
+                        {bnbBalance.slice(0, 4)} BNB available
                       </span>
                     </div>
                   </div>
-                  <span className="mb-1">{bnbBalance} BNB available</span>
                 </div>
               </div>
             </div>
@@ -124,45 +126,43 @@ const ClamMintModal = ({
           <Divider />
 
           {/* output */}
-          <div className="bg-white border-2 shadow-xl rounded-lg">
-            <div className="px-2 py-1">
+          <div className="bg-white border-2 shadow-xl rounded-xl">
+            <div className="px-2 py-2">
               <div className="flex flex-col">
-                <h4 className="text-lg font-semibold">Clams to buy</h4>
+                <h4 className="text-lg font-semibold mb-2">Clams to buy</h4>
                 <div className="flex flex-col text-sm text-gray-500">
                   <div className="flex flex-col">
-                    <label
-                      htmlFor="price"
-                      className="mb-1 uppercase text-grey-darker text-xs font-bold"
-                    >
-                      amount
-                    </label>
-                    <div className="flex flex-row">
-                      <input
-                        disabled
-                        value="1"
-                        className="bg-grey-lighter text-grey-darker p-2 font-normal rounded text-grey-darkest border border-grey-lighter rounded-l-none font-bold"
-                        {...register("output", { required: true })}
-                        // onChange={(v) => {
-                        //   const output = parseUnits(
-                        //     v.currentTarget.value,
-                        //     "ether"
-                        //   ); // input in wei
-                        //   const price = parseUnits(
-                        //     presaleState.salePrice,
-                        //     "ether"
-                        //   ); // input in ether
-                        //   console.log({ output, price });
-                        //   const input = formatUnits(output.div(price), 18);
-                        //   console.log({ output, price, input });
-                        //   setValue("input", input);
-                        // }}
-                      />
-                      <span className="flex items-center bg-gray-100 rounded rounded-r-none px-3 text-lg font-bold font-sans mx-1">
+                    <div className="flex flex-row items-center justify-between">
+                      <div className="flex">
+                        <img className="h-12 mr-2" src={ClamUnknown} />
+                        <input
+                          disabled
+                          value="1"
+                          className="bg-gray-100 text-center text-xl w-20  text-black p-2 font-normal rounded  border-none rounded-l-none font-extrabold"
+                          {...register("output", { required: true })}
+                          // onChange={(v) => {
+                          //   const output = parseUnits(
+                          //     v.currentTarget.value,
+                          //     "ether"
+                          //   ); // input in wei
+                          //   const price = parseUnits(
+                          //     presaleState.salePrice,
+                          //     "ether"
+                          //   ); // input in ether
+                          //   console.log({ output, price });
+                          //   const input = formatUnits(output.div(price), 18);
+                          //   console.log({ output, price, input });
+                          //   setValue("input", input);
+                          // }}
+                        />
+                      </div>
+                      {/* <span className="flex items-center  px-3 text-lg font-extrabold font-sans mx-1">
                         CLAM
-                      </span>
+                      </span> */}
+
+                      <div className="mx-2">1 CLAM = {salePrice} BNB</div>
                     </div>
                   </div>
-                  <span className="mb-1">1 CLAM = {salePrice} BNB</span>
                 </div>
               </div>
             </div>
