f013c2b087b2259d0f0db8c8160882efdf55749b
Luis <hi@ernane.me>
2021-06-16 20:22:38 -0300

    add loading to presale steps

--- src/views/clam_presale/ClamShowModal.js
+++ src/views/clam_presale/ClamShowModal.js
@@ -20,28 +20,37 @@ const ClamShowModal = ({
   updateCharacter,
 }) => {
   const [clamDna, setClamDna] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(async () => {
     console.log({ clamBalance });
     // clamBalance > 0 means already collected the clam
     if (clamBalance !== "0" && address) {
-      const tokenId = await clamContract.getClamByIndex(address, 0);
-      console.log({ tokenId });
+      try {
+        setIsLoading(true);
 
-      if (tokenId) {
-        const clamData = await clamContract.getClamData(tokenId);
-        console.log({ clamData });
-        if (clamData.dna.length > 1) {
-          setClamDna(clamData.dna);
+        const tokenId = await clamContract.getClamByIndex(address, 0);
+        console.log({ tokenId });
 
-          updateCharacter({
-            name: "diego",
-            action: "clam_presale.congratsConllection.text",
-            button: {
-              text: "Dismiss",
-            },
-          });
+        if (tokenId) {
+          const clamData = await clamContract.getClamData(tokenId);
+          console.log({ clamData });
+          if (clamData.dna.length > 1) {
+            setClamDna(clamData.dna);
+
+            updateCharacter({
+              name: "diego",
+              action: "clam_presale.congratsConllection.text",
+              button: {
+                text: "Dismiss",
+              },
+            });
+          }
         }
+
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(false);
       }
     }
   }, [address]);
@@ -65,7 +74,30 @@ const ClamShowModal = ({
 
         <div className="bg-white flex-1 justify-center  md:flex items-center">
           {clamDna && <Clams3D width={"100%"} height={350} clamDna={clamDna} />}
-          {!clamDna && <h1>There is no Clam to see :-(</h1>}
+          {isLoading && (
+            <>
+              <svg
+                className="animate-spin -ml-1 mr-3 h-5 w-5 text-yello-600"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>
+            </>
+          )}
         </div>
       </Card>
     </>
