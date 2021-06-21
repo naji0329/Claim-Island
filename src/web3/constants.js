bf1bd2b0f2d6ded9d57188fa431e4554e4594c24
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2021-06-21 12:00:55 +0200

    update last tests contracts

--- src/web3/constants.js
+++ src/web3/constants.js
@@ -1,11 +1,11 @@
 export const shellPresaleAddress = "0x28D51F0E6CC2138fB134986423cb7429E713763E";
 export const shellTokenAddress = "0x01c16da6E041Cf203959624Ade1F39652973D0EB";
 
-export const clamPresaleAddress = "0xb3f768D115B234Ac39B21D40Cb5E4D530AdAbea4";
-export const clamNFTAddress = "0xd7515fE7E154722e60085beb452732c99fB168aa";
+export const clamPresaleAddress = "0x9e00DdE8263B46FC67a602b95fbD9A8aE240e58D";
+export const clamNFTAddress = "0x792Ce8Ea57f65C292990e1b2AE86902DDa1f0a2E";
 
-export const rngAddress = "0xb0b2d085c3bd0d0abc23f86190d3fd94e1d764a0";
-export const dnaDecoderAddress = "0x6A9A03327b9A9745a6E8b8604FD6aBA62d2f8afd";
+export const rngAddress = "0xbacdF6F20DC405574C7c1e1a7cbAB06107d94DEc";
+export const dnaDecoderAddress = "0xb9cCd96b2D6B9733b97Ad17BA4395Ee37ef0312f";
 
 export const communityVotingAddress =
   "0x000F56780E4DC3f619A08EDf84948780be5C83Cf";
