bc40a3f0cf6d64db8586be88beb64c7345680df4
DkNinja21 <cr.dkninja21@gmail.com>
2021-06-21 22:32:23 +1000

    Revert "final front end fixes"
    This reverts commit c61f2dee31731543f5dee2460ae1d73b4e5456e6.

--- src/views/clam_presale/index.js
+++ src/views/clam_presale/index.js
@@ -24,42 +24,27 @@ const ClamPresale = ({
 
     if (isStarted) {
       if (address) {
-        if (rng) {
-          updateCharacter({
-            name: "diego",
-            action: "clam_presale.collection.text",
-            button: false,
-          });
-        } else if (hasPurchasedClam) {
-          updateCharacter({
-            name: "diego",
-            action: "clam_presale.congratCollection.text",
-            button: false,
-          });
-        } else {
-          updateCharacter({
-            name: "diego",
-            action: "clam_presale.welcome_connected.text",
-            button: {
-              text: "Yes",
-              alt: {
-                action: "cb",
-                destination: () => {
-                  setShowMintModal(true);
-                  updateCharacter({
-                    name: "diego",
-                    action: "clam_presale.purchase.text",
-                    button: {
-                      text: null,
-                    },
-                  });
-                },
+
+        updateCharacter({
+          name: "diego",
+          action: "clam_presale.welcome_connected.text",
+          button: {
+            text: "Yes",
+            alt: {
+              action: "cb",
+              destination: () => {
+                setShowMintModal(true);
+                updateCharacter({
+                  name: "diego",
+                  action: "clam_presale.purchase.text",
+                  button: {
+                    text: null,
+                  },
+                });
               },
             },
-          });
-
-        }
-
+          },
+        });
       } else {
         updateCharacter({
           name: "diego",
