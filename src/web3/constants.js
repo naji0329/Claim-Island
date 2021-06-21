15cdd7014b58cd052c345d26c48dfc2c625fe9fe
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2021-06-21 13:53:46 +0200

    update with actual contracts

--- src/web3/constants.js
+++ src/web3/constants.js
@@ -1,8 +1,8 @@
 export const shellPresaleAddress = "0x28D51F0E6CC2138fB134986423cb7429E713763E";
 export const shellTokenAddress = "0x01c16da6E041Cf203959624Ade1F39652973D0EB";
 
-export const clamPresaleAddress = "0x9e00DdE8263B46FC67a602b95fbD9A8aE240e58D";
-export const clamNFTAddress = "0x792Ce8Ea57f65C292990e1b2AE86902DDa1f0a2E";
+export const clamPresaleAddress = "0x11589a6c845F1Ec9b3bC1a78d3199004875C231B";
+export const clamNFTAddress = "0x77AB76CC8193cafBe7Ec748d52e57b25D184b311";
 
 export const rngAddress = "0xbacdF6F20DC405574C7c1e1a7cbAB06107d94DEc";
 export const dnaDecoderAddress = "0xb9cCd96b2D6B9733b97Ad17BA4395Ee37ef0312f";
