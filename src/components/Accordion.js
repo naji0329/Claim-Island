f444f67886331e5b61912ac9770e2efec6172b2d
Luis <hi@ernane.me>
2021-07-05 21:45:50 -0300

    create accordion component

---
+++ src/components/Accordion.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import classNames from "classnames";
+
+export default ({ data, className }) => {
+  const [tab, setTab] = useState(false);
+
+  return (
+    <>
+      <ul className={`${className} flex flex-col`}>
+        {data &&
+          data.map((row, i) => (
+            <li key={`accordion-${i}`} className="bg-white my-2">
+              <h2
+                className="flex flex-row justify-between items-center font-semibold p-3 cursor-pointer rounded-xl border border-gray-400"
+                onClick={() => {
+                  i === tab ? setTab(-1) : setTab(i);
+                }}
+              >
+                <span>{row.title}</span>
+                <svg
+                  className={classNames(
+                    "fill-current text-blue-700 h-6 w-6 transform transition-transform duration-500",
+                    { "rotate-180": i === tab }
+                  )}
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M13.962,8.885l-3.736,3.739c-0.086,0.086-0.201,0.13-0.314,0.13S9.686,12.71,9.6,12.624l-3.562-3.56C5.863,8.892,5.863,8.611,6.036,8.438c0.175-0.173,0.454-0.173,0.626,0l3.25,3.247l3.426-3.424c0.173-0.172,0.451-0.172,0.624,0C14.137,8.434,14.137,8.712,13.962,8.885 M18.406,10c0,4.644-3.763,8.406-8.406,8.406S1.594,14.644,1.594,10S5.356,1.594,10,1.594S18.406,5.356,18.406,10 M17.521,10c0-4.148-3.373-7.521-7.521-7.521c-4.148,0-7.521,3.374-7.521,7.521c0,4.147,3.374,7.521,7.521,7.521C14.148,17.521,17.521,14.147,17.521,10"></path>
+                </svg>
+              </h2>
+
+              <div
+                className={classNames(
+                  "overflow-hidden duration-500 transition-all",
+                  { "h-full": i === tab },
+                  { "h-0": i !== tab }
+                )}
+              >
+                <p className="p-3 text-gray-900">{row.description}</p>
+              </div>
+            </li>
+          ))}
+      </ul>
+    </>
+  );
+};
