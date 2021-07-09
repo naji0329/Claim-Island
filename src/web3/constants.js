b22cf0ebb0b3547a05e1a474e1570713f1790171
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2021-07-09 23:25:25 +0200

    update codebase with real clamSale v2

--- src/web3/constants.js
+++ src/web3/constants.js
@@ -1,7 +1,7 @@
 export const shellPresaleAddress = "0x28D51F0E6CC2138fB134986423cb7429E713763E";
 export const shellTokenAddress = "0x01c16da6E041Cf203959624Ade1F39652973D0EB";
 
-export const clamPresaleAddress = "0xE2813C0FB130611454F194404fF741Eeb9bFC314";
+export const clamPresaleAddress = "0xAAEB1Ea585DbeF06349ac371EBBA54efa0713D1D";
 export const clamNFTAddress = "0x792Ce8Ea57f65C292990e1b2AE86902DDa1f0a2E";
 
 export const rngAddress = "0xbacdF6F20DC405574C7c1e1a7cbAB06107d94DEc";
