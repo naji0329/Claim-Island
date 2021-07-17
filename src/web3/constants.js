e70514c0712d6eb4f88bbe3bd31d5b45269d8dde
0xIvan <0xivanivan@gmail.com>
2021-07-17 19:22:16 +0200

    Add addresses

--- src/web3/constants.js
+++ src/web3/constants.js
@@ -9,3 +9,10 @@ export const dnaDecoderAddress = "0xb9cCd96b2D6B9733b97Ad17BA4395Ee37ef0312f";
 
 export const communityVotingAddress =
   "0x000F56780E4DC3f619A08EDf84948780be5C83Cf";
+
+export const carapaceTokenAddress = // dummy for ShellToken
+  "0x2db46224672c3a997f5bf51ade9c618434992c94";
+export const jadeTokenAddress = "0xE26482b00781b7BA03f725719feD8Bae1d070f8e"; // dummy for GemToken
+
+export const gemLockerAddress = "0x19549174Df876472040310DfD9b54e8e4eB2Ae55";
+export const masterChefAddress = "0x3334df4bB0590cf869a79Eca8cbB278d483d4e9c";
