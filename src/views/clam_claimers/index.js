c83b62ddb49659deb7c61e89ee871d1d2ab67c63
Luis <hi@ernane.me>
2021-07-27 21:30:39 -0300

    add new card to tell the user to wait for RNG(oracle data)

--- src/views/clam_claimers/index.js
+++ src/views/clam_claimers/index.js
@@ -3,6 +3,8 @@ import { connect } from "redux-zero/react";
 import "./index.scss";
 import CharacterWrapper from "../../components/characters/CharacterWrapper";
 import Web3Navbar from "../../components/Web3Navbar";
+import ClamUnknown from "../../assets/img/clam_unknown.png";
+import Card from "../../components/Card";
 // import Shop from "../../assets/locations/shop_animated.mp4";
 
 import { actions } from "../../store/redux";
@@ -11,6 +13,68 @@ import ClamMintModal from "./ClamMintModal";
 import ClamCollectModal from "./ClamCollectModal";
 // import ClamShowModal from "./ClamShowModal";
 import Web3ClamClaimers from "./Web3ClamClaimers";
+
+const ClamWaitingOracle = () => (
+  <Card>
+    <div className="flex flex-col mb-1">
+      <h2 className="text-blue-700 font-semibold text-2xl mb-2">
+        Get Clams on BSC
+      </h2>
+
+      <div className="alert alert-warning">
+        <div className="flex-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="w-6 h-6 mx-2 stroke-current"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            ></path>
+          </svg>
+          <label>If takes too long please refresh this page</label>
+        </div>
+      </div>
+    </div>
+
+    <div className="bg-white flex-1 justify-center  md:flex items-center">
+      <img src={ClamUnknown} />
+    </div>
+
+    <div className="py-2 flex flex-col">
+      <button
+        type="button"
+        className="flex justify-content-center items-center block uppercase text-center shadow bg-yellow-200 text-yellow-600 text-xl py-3 px-10 rounded-xl cursor-not-allowed"
+      >
+        <svg
+          className="animate-spin -ml-1 mr-3 h-5 w-5 text-yellow-600"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            strokeWidth="4"
+          ></circle>
+          <path
+            className="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          ></path>
+        </svg>{" "}
+        Waiting data from oracle...
+      </button>
+    </div>
+  </Card>
+);
 
 const ClamPresale = ({
   account: { clamBalance, address },
@@ -214,6 +278,8 @@ const ClamPresale = ({
             !hashRequest &&
             !rng && <ClamMintModal setShowMintModal={setShowMintModal} />}
           {/* !rng = did not have clams to collect */}
+
+          {hashRequest && rng && rng === "0" && <ClamWaitingOracle />}
           {hashRequest && rng && Number(rng) > 0 && (
             <ClamCollectModal setShowMintModal={setShowMintModal} />
           )}
