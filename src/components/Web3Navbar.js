86a3dc98431af1ffef4e0fec4f6203f915b5bd04
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2021-08-01 00:42:35 +0200

    re-add absolute to navbar

--- src/components/Web3Navbar.js
+++ src/components/Web3Navbar.js
@@ -172,7 +172,7 @@ const Web3Navbar = ({ updateAccount, ...redux }) => {
         </>
       )}
 
-      <nav className="flex min-h-48 min-w-full items-center px-6 py-4 bg-transparent">
+      <nav className="flex min-h-48 min-w-full items-center absolute px-6 py-4 bg-transparent nav-info-button">
         {/* this push menu to right side */}
         <div className="flex justify-between lg:w-auto w-full  pl-6 pr-2 pb-0 lg:pb-2"></div>
 
@@ -202,7 +202,7 @@ const Web3Navbar = ({ updateAccount, ...redux }) => {
                      }
                   </span>
                 </div>
-                {Number(activateClamBalance) > 0 &&
+                {/* {Number(activateClamBalance) > 0 &&
                   location.pathname.indexOf("saferoom") === -1 && (
                     <div className="flex lg:mt-0 px-4 py-2 mr-2 rounded-xl shadow bg-gray-600 bg-opacity-80">
                       <span className="p-1 text-sm text-gray-200 font-bold font-sans">
@@ -211,7 +211,7 @@ const Web3Navbar = ({ updateAccount, ...redux }) => {
                         </Link>
                       </span>
                     </div>
-                  )}
+                  )} */}
 
                 <div className="flex lg:mt-0 px-4 py-2 bg-gray-900 mr-2 rounded-xl shadow bg-black bg-opacity-80">
                   <div className="p-1 text-sm text-gray-200">
