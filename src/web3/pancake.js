58d3ce9d4b421ec807ec37a16f4c5d444ccd8195
Luis <hi@ernane.me>
2021-08-17 21:28:56 -0300

    create pankcake swap web3 connector

---
+++ src/web3/pancake.js
@@ -0,0 +1,21 @@
+import pancakeAbi from "./abi/pancakeSwap.json";
+
+import { contractFactory } from "./index";
+
+const pancake = (lpAddress) =>
+  contractFactory({
+    abi: pancakeAbi,
+    address: lpAddress,
+  });
+
+export const getLPTokens = (lpAddress) => {
+  const contract = pancake(lpAddress);
+  return Promise.all([
+    contract.methods.token0().call(),
+    contract.methods.token1().call(),
+  ]);
+};
+
+export default {
+  getLPTokens,
+};
