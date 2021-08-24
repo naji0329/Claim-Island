eb649e9c8749d6f443e2474f6cc1aff7e06864b2
0xIvan <0xivanivan@gmail.com>
2021-08-24 23:21:08 +0200

    Implement the right eta for clam deposit

--- src/views/farms/ClamDeposit.js
+++ src/views/farms/ClamDeposit.js
@@ -5,39 +5,49 @@ const ClamDeposit = ({ setSelectedClam, clams }) => {
   return (
     <div className="ClamDeposit">
       <h3 className="heading">Choose a Clam</h3>
-      {clams.map((clam, i) => (
-        <div key={i} className="clam-details">
-          <div className="clam-img">
-            <img src={FarmPearl} />
-          </div>
-          <div className="details">
-            <div className="grid md:grid-cols-2 md:grid-rows-2 gap-4 flex-2">
-              <div className="grid-title">Pearl ETA</div>
-              <div className="grid-value">{clam.pearlETA || "13h 11min"}</div>
-              <div className="grid-title">Lifespan</div>
-              <div className="grid-value">{clam.lifespan || "24h 15min"}</div>
+      {clams.map((clam, i) => {
+        const { birthTime, pearlProductionDelay, pearlProductionStart } = clam.clamDataValues;
+        const etaSeconds =
+          +pearlProductionDelay +
+          (+pearlProductionStart > 0 ? +pearlProductionStart : +birthTime) -
+          new Date().getTime() / 1000;
+
+        return (
+          <div key={i} className="clam-details">
+            <div className="clam-img">
+              <img src={FarmPearl} />
             </div>
-            <div className="flex flex-col">
-              <Link
-                to={"/saferoom"}
-                className="font-montserrat underline"
-                style={{ color: "#757575" }}
-              >
-                View in saferoom
-              </Link>
-              <a
-                className="font-montserrat underline font-bold"
-                style={{ color: "#0072E3" }}
-                onClick={() => {
-                  setSelectedClam(clam);
-                }}
-              >
-                Deposit
-              </a>
+            <div className="details">
+              <div className="grid md:grid-cols-2 md:grid-rows-2 gap-4 flex-2">
+                <div className="grid-title">Pearl ETA</div>
+                <div className="grid-value">
+                  {new Date(etaSeconds * 1000).toISOString().substr(11, 8)}
+                </div>
+                <div className="grid-title">Lifespan</div>
+                <div className="grid-value">{clam.dnaDecoded.lifespan} pearls</div>
+              </div>
+              <div className="flex flex-col">
+                <Link
+                  to={"/saferoom"}
+                  className="font-montserrat underline"
+                  style={{ color: "#757575" }}
+                >
+                  View in saferoom
+                </Link>
+                <a
+                  className="font-montserrat underline font-bold"
+                  style={{ color: "#0072E3", cursor: "pointer" }}
+                  onClick={() => {
+                    setSelectedClam(clam);
+                  }}
+                >
+                  Deposit
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 };
