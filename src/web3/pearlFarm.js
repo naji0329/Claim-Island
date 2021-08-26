6181ca4412f1b1eb97964510ffefc632d5b5b94f
Luis <hi@ernane.me>
2021-08-25 22:10:11 -0300

    fix comparison of gem balance and pearl price

--- src/web3/pearlFarm.js
+++ src/web3/pearlFarm.js
@@ -5,6 +5,7 @@ import { store } from "../store/redux";
 import { approveContractForMaxUintErc721 } from "./bep20";
 import { getBalance, approveSpending } from "./gem";
 import { formatFromWei } from "./shared";
+import BigNumber from "bignumber.js";
 
 const pearlFarm = () =>
   contractFactory({
@@ -22,18 +23,26 @@ const stakePrice = async () => {
   return await pearlFarm().methods.pearlPrice().call();
 };
 
-export const getStakedClamIds = async (account) => {
-  return await pearlFarm().methods.getStakedClamIds(account).call();
+export const getStakedClamIds = (account) => {
+  return pearlFarm().methods.getStakedClamIds(account).call();
 };
 
 export const stakeClam = async (clamId) => {
   const account = getAccount();
-  const gemBal = await getBalance(account);
-  const pearlPrice = await stakePrice();
-  if (gemBal <= pearlPrice)
-    throw new Error(`You need at least ${formatFromWei(pearlPrice)} GEM to stake Clam`);
+  const gemBalance = await getBalance(account).then((v) => new BigNumber(v)); // from string to BN
+  const pearlPrice = await stakePrice().then((v) => new BigNumber(v)); // from string to BN
 
-  await approveContractForMaxUintErc721(account, clamNFTAddress, pearlFarmAddress, clamId);
+  if (gemBalance.isLessThanOrEqualTo(pearlPrice))
+    throw new Error(
+      `You need at least ${formatFromWei(pearlPrice)} GEM to stake Clam`
+    );
+
+  await approveContractForMaxUintErc721(
+    account,
+    clamNFTAddress,
+    pearlFarmAddress,
+    clamId
+  );
   await approveSpending(account, pearlFarmAddress, pearlPrice);
 
   const method = pearlFarm().methods.stakeClam(clamId);
