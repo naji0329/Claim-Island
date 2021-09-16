7174dcc0af25cb57c07ba4a4743cf6149452e4e9
0xIvan <0xivanivan@gmail.com>
2021-09-16 14:37:48 +0200

    Make whole pool clickable

--- src/views/bank/PoolItem.js
+++ src/views/bank/PoolItem.js
@@ -72,98 +72,96 @@ const PoolItem = ({
   };
 
   return (
-    <>
-      <div className="flex flex-col justify-between mb-4 overflow-hidden bg-white border-2 border-gray-800 shadow-md rounded-xl">
-        <div className="flex items-center justify-between px-4 py-1">
-          <div className="flex items-center justify-start min-w-xs">
-            <div className="-space-x-6 avatar-group">
-              {pool.images &&
-                pool.images.map((image, i) => (
-                  <div className="bg-white avatar" key={i}>
-                    <div className="w-12 h-12">
-                      <img src={image} />
-                    </div>
+    <div className="flex flex-col justify-between mb-4 overflow-hidden bg-white border-2 border-gray-800 shadow-md rounded-xl">
+      <div className="flex items-center justify-between px-4 py-1" onClick={handleClick}>
+        <div className="flex items-center justify-start min-w-xs cursor-pointer">
+          <div className="-space-x-6 avatar-group">
+            {pool.images &&
+              pool.images.map((image, i) => (
+                <div className="bg-white avatar" key={i}>
+                  <div className="w-12 h-12">
+                    <img src={image} />
                   </div>
-                ))}
-            </div>
-            <div className="mx-2 text-xl font-aristotelica-bold">{pool.name}</div>
+                </div>
+              ))}
           </div>
+          <div className="mx-2 text-xl font-aristotelica-bold">{pool.name}</div>
+        </div>
 
-          <div className="block text-sm">
-            <Tooltip text="Relative to other investment pools - higher means more risk of capital value fluctuation">
-              <p className={riskClass(pool.risk)}>{pool.risk}</p>
-            </Tooltip>
-          </div>
-          <div className="block text-sm">
-            <p className="mb-1 text-xs font-semibold leading-none text-gray-500">Reward Share</p>
-            <div className="font-bold text-center text-black">
-              {pool.multiplier}%
-              <InfoTooltip text="The share of overall rewards allocated to this pool" />
-            </div>
-          </div>
-
-          <div className="block text-sm">
-            <p className="mb-1 text-xs font-semibold leading-none text-center text-gray-500">APR</p>
-
-            <div className="items-center font-bold text-black">
-              {pool.apr ? renderPercentage(+pool.apr, 2) : "loading..."}
-              <InfoTooltip text="Annual Percentage Return - non-compounded rate of return" />
-            </div>
-          </div>
-
-          <div className="block text-sm">
-            <p className="mb-1 text-xs font-semibold leading-none text-gray-500">Deposited</p>
-            <p className="font-bold text-center text-gray-300">
-              {renderUsd(pool.userDepositAmountInPool * pool.tokenPrice)}
-            </p>
-          </div>
-
-          <div className="block text-sm">
-            <p className="mb-1 text-xs font-semibold leading-none text-gray-500">$GEM to harvest</p>
-            <p className="font-bold text-center text-gray-300">{pool.userRewardAmountInPool}</p>
-          </div>
-
-          <div className="block text-sm">
-            {address ? (
-              <button onClick={handleClick}>
-                <svg
-                  className={classnames(
-                    "fill-current text-blue-700 h-6 w-6 transform transition-transform duration-500",
-                    { "rotate-180": isAdditionalInfoVisible }
-                  )}
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M13.962,8.885l-3.736,3.739c-0.086,0.086-0.201,0.13-0.314,0.13S9.686,12.71,9.6,12.624l-3.562-3.56C5.863,8.892,5.863,8.611,6.036,8.438c0.175-0.173,0.454-0.173,0.626,0l3.25,3.247l3.426-3.424c0.173-0.172,0.451-0.172,0.624,0C14.137,8.434,14.137,8.712,13.962,8.885 M18.406,10c0,4.644-3.763,8.406-8.406,8.406S1.594,14.644,1.594,10S5.356,1.594,10,1.594S18.406,5.356,18.406,10 M17.521,10c0-4.148-3.373-7.521-7.521-7.521c-4.148,0-7.521,3.374-7.521,7.521c0,4.147,3.374,7.521,7.521,7.521C14.148,17.521,17.521,14.147,17.521,10"></path>
-                </svg>
-              </button>
-            ) : (
-              <p
-                className="mb-1 text-xs font-semibold leading-none text-gray-500"
-                onClick={() => activateBrowserWallet()}
-              >
-                Connect Wallet
-              </p>
-            )}
+        <div className="block text-sm">
+          <Tooltip text="Relative to other investment pools - higher means more risk of capital value fluctuation">
+            <p className={riskClass(pool.risk)}>{pool.risk}</p>
+          </Tooltip>
+        </div>
+        <div className="block text-sm">
+          <p className="mb-1 text-xs font-semibold leading-none text-gray-500">Reward Share</p>
+          <div className="font-bold text-center text-black">
+            {pool.multiplier}%
+            <InfoTooltip text="The share of overall rewards allocated to this pool" />
           </div>
         </div>
 
-        {isAdditionalInfoVisible && (
-          <div className="flex items-start justify-between p-4 border-t-2 border-gray-700 h-96">
-            <div className="flex w-1/5">
-              <PoolData urlForExchange={urlForExchange} tvl={pool.tvl} />
-            </div>
+        <div className="block text-sm">
+          <p className="mb-1 text-xs font-semibold leading-none text-center text-gray-500">APR</p>
 
-            <div className="flex w-2/5 h-full">
-              <PoolDepositWithdraw depositFee={depositFee} />
-            </div>
-
-            <div className="flex w-2/5 h-full">
-              <PoolHarvest toggleModal={toggleModal} harvestAmount={pool.userRewardAmountInPool} />
-            </div>
+          <div className="items-center font-bold text-black">
+            {pool.apr ? renderPercentage(+pool.apr, 2) : "loading..."}
+            <InfoTooltip text="Annual Percentage Return - non-compounded rate of return" />
           </div>
-        )}
+        </div>
+
+        <div className="block text-sm">
+          <p className="mb-1 text-xs font-semibold leading-none text-gray-500">Deposited</p>
+          <p className="font-bold text-center text-gray-300">
+            {renderUsd(pool.userDepositAmountInPool * pool.tokenPrice)}
+          </p>
+        </div>
+
+        <div className="block text-sm">
+          <p className="mb-1 text-xs font-semibold leading-none text-gray-500">$GEM to harvest</p>
+          <p className="font-bold text-center text-gray-300">{pool.userRewardAmountInPool}</p>
+        </div>
+
+        <div className="block text-sm">
+          {address ? (
+            <button>
+              <svg
+                className={classnames(
+                  "fill-current text-blue-700 h-6 w-6 transform transition-transform duration-500",
+                  { "rotate-180": isAdditionalInfoVisible }
+                )}
+                viewBox="0 0 20 20"
+              >
+                <path d="M13.962,8.885l-3.736,3.739c-0.086,0.086-0.201,0.13-0.314,0.13S9.686,12.71,9.6,12.624l-3.562-3.56C5.863,8.892,5.863,8.611,6.036,8.438c0.175-0.173,0.454-0.173,0.626,0l3.25,3.247l3.426-3.424c0.173-0.172,0.451-0.172,0.624,0C14.137,8.434,14.137,8.712,13.962,8.885 M18.406,10c0,4.644-3.763,8.406-8.406,8.406S1.594,14.644,1.594,10S5.356,1.594,10,1.594S18.406,5.356,18.406,10 M17.521,10c0-4.148-3.373-7.521-7.521-7.521c-4.148,0-7.521,3.374-7.521,7.521c0,4.147,3.374,7.521,7.521,7.521C14.148,17.521,17.521,14.147,17.521,10"></path>
+              </svg>
+            </button>
+          ) : (
+            <p
+              className="mb-1 text-xs font-semibold leading-none text-gray-500"
+              onClick={() => activateBrowserWallet()}
+            >
+              Connect Wallet
+            </p>
+          )}
+        </div>
       </div>
-    </>
+
+      {isAdditionalInfoVisible && (
+        <div className="flex items-start justify-between p-4 border-t-2 border-gray-700 h-96">
+          <div className="flex w-1/5">
+            <PoolData urlForExchange={urlForExchange} tvl={pool.tvl} />
+          </div>
+
+          <div className="flex w-2/5 h-full">
+            <PoolDepositWithdraw depositFee={depositFee} />
+          </div>
+
+          <div className="flex w-2/5 h-full">
+            <PoolHarvest toggleModal={toggleModal} harvestAmount={pool.userRewardAmountInPool} />
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
 
