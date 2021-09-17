4e48bf7002091f5b35860c9a819b4307533ae262
0xIvan <0xivanivan@gmail.com>
2021-09-18 00:40:11 +0200

    Add helper function to format duration

--- src/utils/time.js
+++ src/utils/time.js
@@ -1,2 +1,14 @@
+import moment from "moment";
+
 export const secondsToFormattedTime = (seconds) =>
   seconds > 0 ? new Date(seconds * 1000).toISOString().substr(11, 8) : "00:00:00";
+
+export const formatMsToDuration = (remainingMs) => {
+  const days = moment.duration(remainingMs).days();
+  const hours = moment.duration(remainingMs).hours();
+  const minutes = moment.duration(remainingMs).minutes();
+  const seconds = moment.duration(remainingMs).seconds();
+  const duration = `${days}d, ${hours}h, ${minutes}m, ${seconds}s`;
+
+  return duration;
+};
