70d8a3954d3b91f9d137dcdd6ad2452ee9cf22a5
0xIvan <0xivanivan@gmail.com>
2021-09-22 00:30:17 +0200

    Merge changes

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -24,29 +24,26 @@ export const Modal = ({ isShowing, onClose, className, children, width, title })
             style={{ background: `rgba(0,0,0,.7)`, zIndex: 1000 }}
           >
             <div
-              className="border border-blue-500 bg-white w-2/3 md:max-w-11/12 mx-auto rounded-xl shadow-lg z-50 overflow-y-auto"
+              className="border border-blue-500 shadow-lg bg-white w-2/3 md:max-w-11/12 mx-auto rounded-2xl z-50 overflow-y-auto"
               style={{ width: width ? width : undefined }}
             >
-              <div className="py-4 text-left px-6">
-                <div className="flex justify-between items-center pb-3">
-                  {title ? <p className="text-2xl font-bold text-gray-500">{title}</p> : <div />}
-                  <div className="cursor-pointer z-50" onClick={onClose}>
-                    <svg
-                      className="fill-current text-gray-500"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="30"
-                      height="30"
-                      viewBox="0 0 20 20"
-                    >
-                      <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
-                    </svg>
-                  </div>
-                  <div className=" flex flex-col text-center text-gray-600 font-aristotelica-bold text-3xl">
-                    {title}
-                  </div>
-
-                  <div className={`${className}`}>{children}</div>
+              <div className="py-4 px-6">
+                <div className="cursor-pointer z-50 flex flex-row-reverse" onClick={onClose}>
+                  <svg
+                    className="fill-current text-gray-500"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="30"
+                    height="30"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+                  </svg>
                 </div>
+                <div className=" flex flex-col text-center text-gray-600 font-aristotelica-bold text-3xl">
+                  {title}
+                </div>
+
+                <div className={`${className}`}>{children}</div>
               </div>
             </div>
           </div>
