d4f891af0704ecc727aba77a091a6df0fa6cd2fb
0xIvan <0xivanivan@gmail.com>
2021-09-21 23:47:01 +0200

    Update contracts only if abi is filled

--- src/updateContractAbis.js
+++ src/updateContractAbis.js
@@ -6,7 +6,7 @@ const main = () => {
   fs.readdirSync(copyFrom).forEach((fileName) => {
     const contract = fs.readFileSync(copyFrom + fileName, "utf8");
     const abi = JSON.parse(contract).abi;
-    fs.writeFileSync(copyTo + fileName, JSON.stringify(abi, null, 2));
+    if (abi.length) fs.writeFileSync(copyTo + fileName, JSON.stringify(abi, null, 2));
   });
 };
 
