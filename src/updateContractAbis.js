f04a06b995a1058c5a493693bebf8f8e98fa4c3f
0xIvan <0xivanivan@gmail.com>
2021-11-18 15:55:03 +0100

    Update contracts

--- src/updateContractAbis.js
+++ src/updateContractAbis.js
@@ -1,13 +1,30 @@
 const fs = require("fs");
 
+const filterContracts = (fileName) => {
+  const lowerCaseName = fileName.toLowerCase();
+
+  if (
+    lowerCaseName.includes("mock") ||
+    lowerCaseName.includes("debugging") ||
+    lowerCaseName.includes("deprecated")
+  ) {
+    return false;
+  }
+
+  return true;
+};
+
 const main = () => {
   const copyFrom = "src/contracts/";
   const copyTo = "src/web3/abi/";
-  fs.readdirSync(copyFrom).forEach((fileName) => {
-    const contract = fs.readFileSync(copyFrom + fileName, "utf8");
-    const abi = JSON.parse(contract).abi;
-    if (abi.length) fs.writeFileSync(copyTo + fileName, JSON.stringify(abi, null, 2));
-  });
+  fs.readdirSync(copyFrom)
+    .filter(filterContracts)
+    .forEach((fileName) => {
+      const contract = fs.readFileSync(copyFrom + fileName, "utf8");
+      const abi = JSON.parse(contract).abi;
+      // exclude empty contracts
+      if (abi.length) fs.writeFileSync(copyTo + fileName, JSON.stringify(abi, null, 2));
+    });
 };
 
 main();
