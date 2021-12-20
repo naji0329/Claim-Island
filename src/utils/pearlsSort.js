c5a7b8f870b17b2db3d3f97e5b379a5e79522a92
kajaku <kajaku.upgrow@gmail.com>
2021-12-20 20:56:40 +0300

    feat(pearls): add sorting by max yield in

--- src/utils/pearlsSort.js
+++ src/utils/pearlsSort.js
@@ -71,6 +71,17 @@ const sortPearlsByBoostDesc = (pearl1, pearl2) => {
   return +gemBoost2 - +gemBoost1;
 };
 
+const sortPearlsByMaxYieldInAsc = (pearl1, pearl2) => {
+  const gemBoost1 = get(pearl1, ["traitsBeforeMaxBoost"]);
+  const gemBoost2 = get(pearl2, ["traitsBeforeMaxBoost"]);
+  return +gemBoost1 - +gemBoost2;
+};
+const sortPearlsByMaxYieldInDesc = (pearl1, pearl2) => {
+  const gemBoost1 = get(pearl1, ["traitsBeforeMaxBoost"]);
+  const gemBoost2 = get(pearl2, ["traitsBeforeMaxBoost"]);
+  return +gemBoost2 - +gemBoost1;
+};
+
 const SORT_FUNCTIONS = {
   [SORT_PEARLS_OPTIONS.id]: {
     [SORT_ORDERS.asc]: sortPearlsByIdAsc,
@@ -95,6 +106,10 @@ const SORT_FUNCTIONS = {
   [SORT_PEARLS_OPTIONS.boost]: {
     [SORT_ORDERS.asc]: sortPearlsByBoostAsc,
     [SORT_ORDERS.desc]: sortPearlsByBoostDesc,
+  },
+  [SORT_PEARLS_OPTIONS.maxYieldIn]: {
+    [SORT_ORDERS.asc]: sortPearlsByMaxYieldInAsc,
+    [SORT_ORDERS.desc]: sortPearlsByMaxYieldInDesc,
   },
 };
 
