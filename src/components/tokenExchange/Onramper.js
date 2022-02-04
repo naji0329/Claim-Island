7e810a7c3cfd5d1d7999067a7346dbbe58fd962b
kajaku <kajaku.upgrow@gmail.com>
2022-02-04 19:42:21 +0300

    feat(exchange): fix review

--- src/components/tokenExchange/Onramper.js
+++ src/components/tokenExchange/Onramper.js
@@ -3,6 +3,7 @@ import OnramperWidget from "@onramper/widget";
 export const Onramper = () => {
   return (
     <OnramperWidget
+      className="Onramper"
       API_KEY={process.env.REACT_APP_ONRAMPER_API_KEY}
       defaultCrypto="BNB"
       defaultFiat="USD"
