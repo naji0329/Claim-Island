9c9fa7ca7c4e46cb2e76347e2c8786f593fca98e
DkNinja21 <cr.dkninja21@gmail.com>
2022-02-21 03:17:16 +1000

    update withdrawal warning

--- src/components/characters/constants/speeches/bank.js
+++ src/components/characters/constants/speeches/bank.js
@@ -73,7 +73,7 @@ export const BANK_SPEECH = {
       skip: false,
     },
     withdraw_pearl_rewards_alert: {
-      text: "You still have pending Pearl reward boosts available. Withdrawing now will cause your boosted rewards to be reduced proportionally. Are you sure you want to withdraw?",
+      text: "You still have locked yield currently streaming. Withdrawing from this pool will cause all pending streams to be reduced proportionally after 24 hours.To prevent the reduction, you must deposit your withdrawal back within 24 hours. Are you sure you want to withdraw?",
       next: false,
       dismiss: true,
       skip: false,
