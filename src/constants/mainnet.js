412a9510cc4caf273d0bb9d876044e12e9c91e37
0xIvan <0xivanivan@gmail.com>
2022-03-24 14:16:18 +0800

    Address order

--- src/constants/mainnet.js
+++ src/constants/mainnet.js
@@ -18,10 +18,10 @@ export default {
   pearlHuntAddress: "0x3CefCe21F06bA801A97f7167E2292ea38854Df96",
   gemOracleAddress: "0x66Fff147d22e9604aEc34B7C7067e9F5662c9bF0",
   clamExchangeAddress: "0x4faaa3754330313351B41cD21BeEC4A9786b336f",
+  zapAddress: "0x0db5aEf2b07fE1Df1cfD34cd43B411A73E4A0534",
   // third party addresses
   pancakeRouterAddress: "0x10ED43C718714eb63d5aA57B78B54704E256024E",
   wBNB: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
   BUSD: "0xe9e7cea3dedca5984780bafc599bd69add087d56",
   multicallAddress: "0x0AD12d0cF5137e51e82B486381f4a6E4cbddE2F1",
-  zapAddress: "0x0db5aEf2b07fE1Df1cfD34cd43B411A73E4A0534",
 };
